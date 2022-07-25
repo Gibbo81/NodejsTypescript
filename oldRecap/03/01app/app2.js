1ae9d22dd56e60595734af5a0c05bbcd19967f09
Giuliano <masigiuliano@yahoo.it>
2022-07-25 21:14:14 +0200

    complete part 3 recovery node.js

--- oldRecap/03/01app/app2.js
+++ oldRecap/03/01app/app2.js
@@ -1,5 +1,8 @@
+const chalk=require('chalk');
 const externalUser = require('./utils.js')
 const getNotes = require('./note.js')
+const validator = require('validator')//installe from npm before
+
 
 const name = 'Pippus'
 console.log(name)
@@ -7,3 +10,8 @@ console.log('external user' + externalUser.name + ' - '+ externalUser.surname + 
 console.log('Add 2 and 4: ' + externalUser.add(2,4) ) 
 console.log('Second Add 2 and 4: ' + externalUser.add2(2,4) ) 
 console.log(getNotes() ) 
+
+console.log(validator.isEmail('pluto@yov.com'))
+console.log(validator.isEmail('pluto@yov'))
+
+console.log(chalk.bgRed('Success!!!!'));
