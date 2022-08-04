e4c46370260ed8cb29f87e8ea49e55580d1c9f3b
MASI Giuliano <giuliano.masi@alstomgroup.com>
2022-08-04 12:00:27 +0200

    Complete chap 6

--- oldRecap/05/weatherApp/app.js
+++ oldRecap/05/weatherApp/app.js
@@ -5,22 +5,13 @@ const darksky = require('./darksky')
 //call examples:
 //node app.js "New York"
 //node app.js Bologna
+const address = process.argv[2]
+if(!address)
+    return
 
-/*
-old not configurable example
-const lan = 88
-const log = -122
-const urlDarkSky ='https://api.darksky.net/forecast/51c13b1fe2e91b6e6e9b9fff61480e8a/'  + lan + ',' + log + '?units=si&lang=en'
-//json: true -->automatically parse the response
-request({ url: urlDarkSky, json: true }, (error, response) => {    
-    console.log("It's currently " + response.body.currently.temperature + ' there is ' + response.body.currently.precipProbability + '% chance of rain')
-} )
-*/
-
-
-geo(process.argv[2], (long, lat) => {
+geo(address, (long, lat) => {
     darksky(long, lat, (temperature, precipProbability) => {
-        console.log("It's currently " + temperature + ' there is ' + precipProbability + '% chance of rain')})
+        console.log("In " + address +  " it's currently " + temperature + ' there is ' + precipProbability + '% chance of rain')})
     })
 
 
