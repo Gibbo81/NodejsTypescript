f3bcde35f9d373d07388577bf6889da0f1c8a472
MASI Giuliano <giuliano.masi@alstomgroup.com>
2022-09-05 18:43:13 +0200

    complete chap 16.5

--- oldRecap/16/autoTesting/tests/math.test.js
+++ oldRecap/16/autoTesting/tests/math.test.js
@@ -1,8 +1,52 @@
-
 //to start => npm test
+//https://jestjs.io/docs/expect
+
+const {tip, fahrenheitToCelsius, celsiusToFahrenheit, add} = require('../math')
 
 test('sum test', () => {
     const total = 5+6;
     if (total !== 11)
         throw new Error('Sum error')
+})
+
+test('default tip is 20%', ()=> {
+    const bill= 100
+    
+    var result = tip(bill)
+
+    expect(result).toBe(120) //https://jestjs.io/docs/expect
+})
+
+test('25% tip works fine', ()=> {
+    const bill= 100
+    const tipPercent = 0.25
+    
+    var result = tip(bill, tipPercent)
+
+    expect(result).toBe(125) 
+})
+
+test('should convert 32 F to 0 C', ()=> {
+    const temperatureF= 32 
+        
+    var result = fahrenheitToCelsius(temperatureF)
+
+    expect(result).toBe(0) 
+})
+
+test('should convert 0 C to 32 F', ()=> {
+    const temperatureC= 0 
+        
+    var result = celsiusToFahrenheit(temperatureC)
+
+    expect(result).toBe(32) 
+})
+
+test('async sum must work', async () => {
+    const x = 10 
+    const y = 65
+        
+    var result = await add(x, y)
+
+    expect(result).toBe(75) 
 })
---
+++ oldRecap/16/autoTesting/tests/second.test.js
@@ -0,0 +1,3 @@
+test('tip is 20%', ()=> {
+    expect(true).toBe(true) //https://jestjs.io/docs/expect
+})
