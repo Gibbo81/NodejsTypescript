ce097ac70a3d78b184f9d46a71c9d4887d9a1915
Giuliano <masigiuliano@yahoo.it>
2022-09-06 16:34:26 +0200

    fix bug on mock

--- oldRecap/16/autoTesting/tests/mockClasses/mockClassMethod_C1.test.js
+++ oldRecap/16/autoTesting/tests/mockClasses/mockClassMethod_C1.test.js
@@ -4,7 +4,7 @@ const methodToMock = require('../../C1')
 test('fine with 10 and 43', async () => {
     methodToMock.f1 = basicMockCreator(53)
     methodToMock.f2 = basicMockCreator(-33)
-    methodToMock.f3 = basicMockCreator(Promise.resolve(430))
+    methodToMock.f3Async = basicMockCreator(Promise.resolve(430))
 
     var result = await classToTest(10,43)
 
