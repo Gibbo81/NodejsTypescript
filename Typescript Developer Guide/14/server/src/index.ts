59159a730edaaa8afbb929f7c9a03b5b177699ba
Giuliano <masigiuliano@yahoo.it>
2022-12-17 20:19:53 +0100

    Complete chap 14.5

--- Typescript Developer Guide/14/server/src/index.ts
+++ Typescript Developer Guide/14/server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi there')
+
+//ctrl+click on express to open the espress types definition file with all the types it exports
+import express, {Request, Response} from 'express' //o import both the full object and some of its submodules
+//not strictly necessry i can use express.Request but it's easier
+import { router } from './routes/loginRoutes'
+
+
+
+
+const app =express()
+app.use(router)
+
+app.listen(3000, ()=>{console.log('listening on port 3000')})
