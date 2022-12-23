3874361854c74b72f9b9cddeb52905340184d160
MASI Giuliano <giuliano.masi@alstomgroup.com>
2022-12-23 13:00:00 +0100

    Complete chap 14

--- Typescript Developer Guide/14/server/src/index.ts
+++ Typescript Developer Guide/14/server/src/index.ts
@@ -1,14 +1,17 @@
 
+//Use express with typescript with the less possible changes. 
+
 //ctrl+click on express to open the espress types definition file with all the types it exports
 import express, {Request, Response} from 'express' //o import both the full object and some of its submodules
 //not strictly necessry i can use express.Request but it's easier
 import { router } from './routes/loginRoutes'
 import bodyParser from 'body-parser'
-
+import cookieSession from 'cookie-session'
 
 
 const app =express()
 app.use(bodyParser.urlencoded({ extended:true})) //in the other course we used app.use(express.json())
+app.use(cookieSession({keys:['oooo']}))
 app.use(router)
 
 app.listen(3000, ()=>{console.log('listening on port 3000')})
--- Typescript Developer Guide/14/server/src/routes/loginRoutes.ts
+++ Typescript Developer Guide/14/server/src/routes/loginRoutes.ts
@@ -1,5 +1,14 @@
-import {Router, Request, Response} from 'express'
+import {Router, Request, Response, NextFunction} from 'express'
 
+
+function requiredAuth(req:Request, res:Response, next : NextFunction) : void {
+    if (req.session && req.session.loggedIn)
+        return next() //procede to next handler
+    else{
+        res.status(403)
+        res.send('forbidden')
+    }
+}
 
 const router = Router()
 
@@ -18,10 +27,46 @@ router.get('/login', (req:Request, res:Response)=> {
         </form>`)
 })
 
+router.get('/logout', (req:Request, res:Response)=> {    
+    if (req.session)
+        req.session.loggedIn=false
+    res.redirect('/')
+})
+
 router.post('/login', (req:Request, res:Response)=> {    
+    //by default req doesn't have a body property, it's only because we added a parser: 'app.use(bodyParser.urlencoded({ extended:true}))'
+    //that we are able to use the body
     const {email,password} = req.body
-    console.log( req.body)
-    res.send(email+password)
+    if(email && password && email==='pi@pi.com' && password === 'pluto'){
+        //as before req does not have a session property by default, we enable it adding: 'app.use(cookieSession({keys:['oooo']}))'  inside index.ts
+        if (req.session)
+            req.session.loggedIn=true
+        else
+            req.session = { loggedIn : true}
+        res.redirect('/')
+    }else {
+        res.send('invalid credential')
+    }
+
+})
+
+router.get('/', (req:Request, res:Response)=> {    
+    if(req.session && req.session.loggedIn)    
+        res.send(`
+            <div>       
+                <div>you are logged in!!</div>                
+                <a href="/logout">logout</a>
+            </div>`)
+    else
+    res.send(`
+    <div>                    
+        <div>you are NOT logged in!</div>
+        <a href="/login">login</a>
+    </div>`)
+})
+
+router.get('/protected', requiredAuth, (req:Request, res:Response)=> {    
+    res.send(`welcom to protected route`)
 })
 
 export {router}
